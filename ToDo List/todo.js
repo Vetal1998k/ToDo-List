bc1923f1595f988216683fb4a20ae132f1745752
Vitaliy Kozyuberdin <koziuberdin.vetal@gmail.com>
2018-10-14 15:11:34 +0300

    Update todo.js

--- ToDo List/todo.js
+++ ToDo List/todo.js
@@ -5,24 +5,27 @@ var model = {
 
 var toDoList = angular.module("toDoList", []);
 
-toDoList.controller("ToDoListCtrl", function($scope){
+toDoList.controller("ToDoListCtrl", function($scope) {
     $scope.list = model;
 
-    $scope.addTask = function(){  
-        $scope.list.points.push({
-            name: $scope.taskName,
-            description: $scope.descriptionText,
-            completed: false
-        });
-        $scope.taskName = "";
-        $scope.descriptionText = "";
+    $scope.addTask = function() { 
+        if (angular.isDefined($scope.taskName) &&
+        angular.isDefined($scope.descriptionText)) {
+            $scope.list.points.push({
+                name: $scope.taskName,
+                description: $scope.descriptionText,
+                completed: false
+            });
+            $scope.taskName = "";
+            $scope.descriptionText = "";
+        }         
     }
 
-    $scope.showText = function(completed){
+    $scope.showText = function(completed) {
         return completed ? "Completed" : "Didn't complete";
     }
 
-    $scope.setStyle = function(completed){
+    $scope.setStyle = function(completed) {
         return completed ? "color: green" : "color: red; font-weight: bold";
     }
 });
